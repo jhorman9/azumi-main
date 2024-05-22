08cbd3baf590679d6f81697f55dd6efde9c58bf3
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-05-21 21:05:15 -0500

    agregando al menu las entradas y las sopas

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import '../assets/styles/main.css';
+import categoryPNG from '../assets/images/image-category.png';
 
 const MainPage = () => {
 
@@ -20,8 +21,66 @@ const MainPage = () => {
         <title>Azumi - Men&#250; digital</title>
       </Helmet>
       <div className='main_entrees'>
-        <span className='title_main'>Entradas</span>
-        
+        <h2 className='title_main'>ENTRADAS</h2>
+        <div className='content__card'>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>Sushi</h3>
+            </div>
+          </div>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>Tailandesa</h3>
+            </div>
+          </div>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>Japonesa</h3>
+            </div>
+          </div>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>China</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className='main_entrees soups'>
+        <h2 className='title_main'>SOPAS</h2>
+        <div className='content__card'>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>Japonesa</h3>
+            </div>
+          </div>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>China</h3>
+            </div>
+          </div>
+
+          <div className='section__card'>
+            <img width={200} height={'100%'} src={categoryPNG} alt="Imagen representativa de la categoria japonesa" title='Men&#250; de la categoria japonesa' className='card__image'/>
+            <div className='card__text'>
+              <h3 className='sub-title_main'>Peruana</h3>
+            </div>
+          </div>
+
+        </div>
       </div>
     </section>
   )
