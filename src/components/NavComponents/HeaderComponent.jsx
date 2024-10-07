18a13d7ea4a6d60011a84c4ea83270328e3e203f
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-10-07 00:36:22 -0500

    insertando nuevas fotos

--- src/components/NavComponents/HeaderComponent.jsx
+++ src/components/NavComponents/HeaderComponent.jsx
@@ -6,14 +6,16 @@ const HeaderComponent = () => {
   return (
     <header>
         <div className='header-info'>
-        <LocationSVG />
-        <a target='_blank' href='https://maps.app.goo.gl/xDAGmWJ54sSaSfoH9'><span className='desktop-view'>Calle 72 Plaza 72 Local 3 -</span> San Francisco</a>
+          <LocationSVG />
+          <a target='_blank' href='https://maps.app.goo.gl/xDAGmWJ54sSaSfoH9'><span className='desktop-view'>Calle 72 Plaza 72 Local 3 -</span> San Francisco</a>
         </div>
         <div className='header-info'>
-        <PhoneSVG />
-        <a href="tel:3904072">390-4072</a>
-        <PhoneSVG />
-        <a href="https://wa.link/egw0dg">+507 6045-4821</a>
+          <PhoneSVG />
+          <a href="tel:3904072">390-4072</a>
+        </div>
+        <div className="header-info">
+          <PhoneSVG />
+          <a href="https://wa.link/egw0dg">+507 6045-4821</a>
         </div>
     </header>
   )
