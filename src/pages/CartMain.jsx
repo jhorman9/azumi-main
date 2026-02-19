2fcc10c33ea5b14e9ce8f304d0e95ed888a51113
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2026-02-19 02:25:30 -0500

    eliminando archivos innecesarios

--- src/pages/CartMain.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CartMain = () => {
-  return (
-    <h1>CartMain</h1>
-  )
-}
-
-export default CartMain;
--- src/pages/ReservaPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Reserva = () => {
-  return (
-    <h1>Reserva</h1>
-  )
-}
-
-export default Reserva;
